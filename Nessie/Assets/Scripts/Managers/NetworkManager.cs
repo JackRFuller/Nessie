8a43273eadb14e2f5eb9689061a40eda2b64d66b
JackRFuller <jackrfuller@outlook.com>
2019-01-09 20:06:26 +0100

    Setup Network Manager and Creating a 1v1 server

---
+++ Nessie/Assets/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using System;
+
+public class NetworkManager : Photon.MonoBehaviour
+{
+    public event Action<int> PlayerJoinedOrCreatedRoom;
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("Pre-Alpha");
+    }
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("ConnectedToMaster");
+    }
+
+    public virtual void JoinOrCreateRoom()
+    {
+          RoomOptions roomOptions = new RoomOptions();
+          roomOptions.MaxPlayers = 2;
+          PhotonNetwork.JoinOrCreateRoom("newRoom",roomOptions,null);      
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        Debug.Log("Player Connected To Room");
+        int numberOfPlayersInRoom = PhotonNetwork.playerList.Length;
+
+        if(numberOfPlayersInRoom > 1)
+        {
+            Debug.Log("Server Filled");
+            Debug.Log("Player One: " + PhotonNetwork.playerList[0].NickName);
+            Debug.Log("Player Two: " + PhotonNetwork.playerList[1].NickName);
+        }
+
+        PlayerJoinedOrCreatedRoom(numberOfPlayersInRoom);
+    }
+
+    public virtual void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        Debug.Log("OnPhotonPlayerConnected");
+        if(PhotonNetwork.playerList.Length == 2)
+        {            
+            Debug.Log("Server Filled");
+            Debug.Log("Player One: " + PhotonNetwork.playerList[0].NickName);
+            Debug.Log("Player Two: " + PhotonNetwork.playerList[1].NickName);
+        }        
+
+        PlayerJoinedOrCreatedRoom(PhotonNetwork.playerList.Length);
+    }
+
+    #region UIMenuFunctions
+
+    public void SetClientPlayerName(string playerName)
+    {
+        PhotonNetwork.player.NickName = playerName;
+    }
+
+    #endregion
+}
---
+++ Nessie/Assets/Scripts/UI/UINetworkMainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UINetworkMainMenu : MonoBehaviour
+{
+    private NetworkManager networkManager;
+
+    [Header("UI Elements")]
+    [SerializeField] private GameObject playerNameInputFieldObj;
+    [SerializeField] private TMP_Text playerNameInputFieldText;
+    
+    [SerializeField] private GameObject connectToRoomButton;
+    [SerializeField] private GameObject waitingForOpponentText;
+
+    private void Start() 
+    {
+        networkManager = GameManager.Instance.NetworkManager;   
+         networkManager.PlayerJoinedOrCreatedRoom += HideInitialConnectToServerUI; 
+        networkManager.PlayerJoinedOrCreatedRoom += ToggleWaitingForOpponentMessage; 
+
+        ToggleWaitingForOpponentMessage(0);
+    }
+
+    public void OnPlayerNameUpdated(string playerName)
+    {
+        networkManager.SetClientPlayerName(playerNameInputFieldText.text);
+    }
+    
+    public void OnClickConnectToGame()
+    {
+        networkManager.JoinOrCreateRoom();
+    }
+
+    private void HideInitialConnectToServerUI(int numberOfPlayers)
+    {
+        playerNameInputFieldObj.SetActive(false);
+        connectToRoomButton.SetActive(false);
+    }
+
+    private void ToggleWaitingForOpponentMessage(int numberOfPlayers)
+    {
+        if(numberOfPlayers == 0 || numberOfPlayers == 2)
+            waitingForOpponentText.SetActive(false);
+        if(numberOfPlayers == 1)
+            waitingForOpponentText.SetActive(true);
+    }
+
+   
+}
